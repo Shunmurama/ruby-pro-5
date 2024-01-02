faa2d0479a1cf0b93a10b9da90bfaa7b7da06f0b
Shunmurama <mura.faily72@gmail.com>
2024-01-02 12:49:20 +0000

    ~p176

--- symbol.rb
+++ symbol.rb
@@ -9,3 +9,45 @@ string = 'apple'
 p string.upcase
 p string
 # シンボルでは使えない
+
+p '主な用途'
+
+currencies = {'japan' =>'yen', 'us' => 'dollar', 'india' => 'rupee'}
+puts currencies['japan']
+# キャッシュでもできる
+currencies = {:japan =>'yen', :us => 'dollar', :india => 'rupee'}
+puts currencies[:us]
+
+puts '---------------------'
+
+
+def sta
+  status = 2
+  case status
+  when 0
+    'これからやります'
+  when 1
+    '今やってます'
+  when 2
+    'もう終わりました'
+  end
+end
+
+puts sta
+
+def sss
+  status = :todo
+  case status
+  when :todo
+    'これからやります'
+  when :doing
+    '今やってます'
+  when :done
+    'もう終わりました'
+  end
+end
+
+puts sss
+
+p '----------------------'
+
